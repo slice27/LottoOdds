a9f8891bf9a73bb8005b5d0d0b50bbd4307af9e7
Chris Chaffey <chris.chaffey@gmail.com>
2017-02-13 15:34:10 -0400

    Adding the makefiles and netbeans projects to build the code.

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=lottoodds
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/lottoodds
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=lottoodds.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/lottoodds.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=lottoodds
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/lottoodds
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=lottoodds.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/lottoodds.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
